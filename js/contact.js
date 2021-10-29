a6814b5e62e7b225e7fe9537e4321c49e9f584c5
doloresbruzzone <dolobruzzone@gmail.com>
2021-10-29 13:33:04 -0300

    update contact form, listoo

--- js/contact.js
+++ js/contact.js
@@ -1,24 +1,27 @@
 
-const crearMensaje = (e) => {
+var form = document.getElementById('contactForm');
+form.addEventListener('submit', function(e){
     e.preventDefault();
+    let inputAsunto = document.querySelector("#asunto");
     let inputNombre = document.querySelector("#nombre");
-    if(inputNombre.ariaValueMax.length == 0) {
-        alert("Por favor ingrese su nombre");
-        return false;
-    }
-}
+    let inputEmail = document.querySelector("#email");
+    let descripcion = document.querySelector("#descripcion");
 
-const renderElement = () =>{
-    let div = document.createElement('div');
-    div.classList.add('box');
-    let html = `
-            <div class="texto">
-                <h2>${asunto}</h2>
-                <p>${nombre}</p>
-                <p>${email}</p>
-                <p>${descripcion}</p>
-            </div>
-    `;
-    div.innerHTML = html;
-    return div;
-}
+    var mensajesError = [];
+    if(inputNombre === null || inputNombre.value.length == 0){
+        mensajesError.push('Ingrese su nombre');
+    }
+    if(inputAsunto === null || inputAsunto.value.length == 0){
+        mensajesError.push('Ingrese un asunto');
+    }
+
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var valido=expReg.test(inputEmail);
+    
+    if(valido==false || inputEmail === null || inputEmail.value.length == 0){
+        mensajesError.push('El email ingresado no es valido');
+    }
+
+    error.style.color = '#ff5b48';
+    error.innerHTML = mensajesError.join(', ');
+});
